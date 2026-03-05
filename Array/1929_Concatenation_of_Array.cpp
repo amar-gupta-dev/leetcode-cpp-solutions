4a7160adf56aed7ccb4dd975bb66c12dbba50b49
amar gupta <amargupta8970@gmail.com>
2026-03-05 11:38:10 +0530

    Fix typo in concatenation of an array
    C++ solution for Concatenation of Array.
    Placed file under Array directory for topic organization.
    Solution runs in O(n) time and uses O(2n) space as required.

---
+++ Array/1929_Concatenation_of_Array.cpp
@@ -0,0 +1,21 @@
+// Leetcode 1929
+// Problem: Cocatenation of Array
+// Topic: Array
+// Difficulty: Easy
+
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> ans(2 * n);
+
+        for(int i = 0; i < n; i++)
+        {
+            ans[i] = nums[i];
+            ans[i + n] = nums[i];
+        }
+
+        return ans;
+    }
+};
