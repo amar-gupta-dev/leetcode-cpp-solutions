264dfcad736724e458ad8e9a6b02d2260d4f20cc
amar gupta <amargupta8970@gmail.com>
2026-03-14 21:02:13 +0530

    Add solution for minimum flips to make a OR b equal to c
    Implement solution for Leetcode problem 1318, calculating minimum flips to make a OR b equal to c using bit manipulation.

---
+++ Bits Manipulation/1318_Minimum_flips_to_make_a_OR_b_equal_to_c.cpp
@@ -0,0 +1,26 @@
+// Leetcode 1318
+// Problem: minimum flips to make a OR b equal to c
+// Topic: bit manipulation
+// Diff: medium
+
+class Solution {
+public:
+    int minFlips(int a, int b, int c) {
+        int flips = 0;
+
+        for(int i = 0; i < 32; i++) {
+            int bitA = (a >> i) & 1;
+            int bitB = (b >> i) & 1;
+            int bitC = (c >> i) & 1;
+
+            if(bitC == 0) {
+                flips += bitA + bitB;
+            }
+            else {
+                if(bitA == 0 && bitB == 0)
+                flips += 1;
+            }
+        }
+        return flips;
+    }
+};
