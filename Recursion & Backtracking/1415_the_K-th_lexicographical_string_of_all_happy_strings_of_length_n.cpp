6b8176a9a4ff95fdc094a98db869ebafdbc4c338
amar gupta <amargupta8970@gmail.com>
2026-03-14 21:09:54 +0530

    Add solution for k-th happy string problem
    Implement solution for Leetcode problem 1415 to find the k-th lexicographical happy string of length n using recursion and backtracking.

---
+++ Recursion & Backtracking/1415_the_K-th_lexicographical_string_of_all_happy_strings_of_length_n.cpp
@@ -0,0 +1,31 @@
+// Leetcode 1415
+// Problem: the k-th lexicographical string of all happy strings of length n
+// Topic: recursion & backtracking
+// Diff: medium
+
+class Solution {
+public:
+
+        void solve(int n, string curr, vector<string>& res) {
+            if(curr.length() == n) {
+                res.push_back(curr);
+                return;
+            }
+
+            for(char ch : {'a','b','c'}) {
+                if(curr.empty() || curr.back() != ch) {
+                    solve(n, curr + ch, res);
+                }
+            }
+
+        }
+    string getHappyString(int n, int k) {
+
+        vector<string> res;
+        solve(n, "", res);
+
+        if(k > res.size()) return "";
+        return res[k-1];
+        
+    }
+};
