acfe94d22e9befbc88e07bed7a36df618cfa0053
amar gupta <amargupta8970@gmail.com>
2026-03-28 21:23:26 +0530

    Add solution for finding string with LCP
    "Greedy string construction + DP validation using LCP matrix."

---
+++ strings/2573_find_the_string_with_LCP.cpp
@@ -0,0 +1,45 @@
+// Leetcode 2573
+// Problem: find the string with LCP
+// Topics: strung, greedy, DP, matrix, string-cunstruction
+// Diff: hard
+
+class Solution {
+public:
+    string findTheString(vector<vector<int>>& lcp) {
+        int n = lcp.size();
+        string s(n, '#');
+
+        char ch = 'a';
+
+        for(int i = 0; i < n; i++) {
+            if(s[i] != '#') continue;
+
+            if(ch > 'z') return "";
+
+            for(int j = i; j < n; j++) {
+                if(lcp[i][j] > 0) {
+                    s[j] = ch;
+                }
+            }
+            ch++;
+        }
+
+        vector<vector<int>> dp(n + 1, vector<int>(n + 1, 0));
+
+        for(int i = n - 1; i >= 0; i--) {
+            for(int j = n - 1; j >= 0; j--) {
+                if(s[i] == s[j]) {
+                    dp[i][j] = dp[i + 1][j + 1] + 1;
+                } else {
+                    dp[i][j] = 0;
+                }
+
+                if(dp[i][j] != lcp[i][j]) {
+                    return "";
+                }
+            }
+        }
+
+        return s;
+    }
+};
