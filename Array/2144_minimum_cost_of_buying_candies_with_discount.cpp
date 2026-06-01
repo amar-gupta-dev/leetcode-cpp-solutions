c0c2d4896ea6c4a6f82aba6dfe2b2e1a9c52e3c1
amar gupta <amargupta8970@gmail.com>
2026-06-01 13:11:03 +0530

    Implement solution for minimum cost of candies problem

---
+++ Array/2144_minimum_cost_of_buying_candies_with_discount.cpp
@@ -0,0 +1,21 @@
+// Leetcode 2144
+// Problem: minimum cost of buying candies with discount
+// Topic: array
+// Diff: easy
+
+
+class Solution {
+public:
+    int minimumCost(vector<int>& cost) {
+        sort(cost.begin(), cost.end(), greater<int>());
+
+        int ans = 0;
+
+        for(int i = 0;  i < cost.size(); i++) {
+            if(i % 3 != 2) {
+                ans += cost[i];
+            }
+        }
+        return ans;
+    }
+};
